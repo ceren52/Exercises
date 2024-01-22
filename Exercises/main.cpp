8b42ceb2bd3daf357175de184a439d70d781f612
fmuge1 <fmugeakcan@gmail.com>
2024-01-22 21:14:49 +0300

    Initial Commit

---
+++ Exercises/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Exercises
+//
+//  Created by CEREN YANIK on 22.01.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
